bc9ec329e53c14472fe5b7822674e4bbb1e86639
Rodrigo Nappi <rodrigo_nappi@hotmail.com>
2020-05-19 19:56:59 -0300

    Fazendo tremer

---
+++ AppVibrate/AppVibrate/ViewModels/AboutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AppVibrate.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+        public ICommand StartVibration()
+        {
+            var duration = TimeSpan.FromSeconds(10);
+            return new Command(() => Vibration.Vibrate(duration));
+        }
+
+        public ICommand StopVibration => new Command(() => Vibration.Cancel());
+    }
+}
